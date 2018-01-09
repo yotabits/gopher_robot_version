09a99d62dd9ead06d4f510639540d9a6e57614f6
Thomas <thomas.kostas@yujinrobot.com>
2018-01-09 19:06:49 +0900

    Version bump + minor fix
    Fixed typo in path to devel binaries

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gopher_version',
-      version='0.11',
+      version='0.12',
       description='Create a file containing the name and version of various ROS and system packages',
       author='Thomas Kostas',
       url='https://github.com/yotabits/gopher_version',
